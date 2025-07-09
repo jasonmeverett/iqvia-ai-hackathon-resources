2b08649bd4761a5dd1b8d58c92fcf70523e3e226
Jason Everett <jev@Jasons-MacBook-Pro.local>
2025-07-09 08:16:39 -0400

    JSON reader

---
+++ tool_templates/json_reader/tool.py
@@ -0,0 +1,76 @@
+"""
+A simple JSON reader tool that can read JSON files relative to the local tool directory.
+"""
+
+from pydantic import BaseModel, Field
+from typing import Optional, Any
+import json 
+import argparse
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parent
+
+
+class UserParameters(BaseModel):
+    pass 
+
+
+class ToolParameters(BaseModel):
+    """
+    Arguments of a tool call. These arguments are passed to this tool whenever
+    an Agent calls this tool. The descriptions below are also provided to agents
+    to help them make informed decisions of what to pass to the tool.
+    """
+    filepath: str = Field(description="First parameter that should be passed to the tool")
+
+
+def run_tool(config: UserParameters, args: ToolParameters) -> Any:
+    """
+    Main tool code logic. Anything returned from this method is returned
+    from the tool back to the calling agent.
+    """
+    
+    filepath = ROOT_DIR / args.filepath
+    data = json.load(open(filepath))
+    return json.dumps(data, indent=2)
+
+
+
+OUTPUT_KEY = "tool_output"
+"""
+When an agent calls a tool, technically the tool's entire stdout can be passed back to the agent.
+However, if an OUTPUT_KEY is present in a tool's main file, only stdout content *after* this key is
+passed to the agent. This allows us to return structured output to the agent while still retaining
+the entire stdout stream from a tool! By default, this feature is enabled, and anything returned
+from the run_tool() method above will be the structured output of the tool.
+"""
+
+
+if __name__ == "__main__":
+    """
+    Tool entrypoint. 
+    
+    The only two things that are required in a tool are the
+    ToolConfiguration and ToolArguments classes. Then, the only two arguments that are
+    passed to a tool entrypoint are "--tool-config" and "--tool-args", respectively. The rest
+    of the implementation is up to the tool builder - feel free to customize the entrypoint to your 
+    chosing!
+    """
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--user-params", required=True, help="Tool configuration")
+    parser.add_argument("--tool-params", required=True, help="Tool arguments")
+    args = parser.parse_args()
+    
+    # Parse JSON into dictionaries
+    user_dict = json.loads(args.user_params)
+    tool_dict = json.loads(args.tool_params)
+    
+    # Validate dictionaries against Pydantic models
+    config = UserParameters(**user_dict)
+    params = ToolParameters(**tool_dict)
+    
+    # Run the tool.
+    output = run_tool(config, params)
+    print(OUTPUT_KEY, output)
