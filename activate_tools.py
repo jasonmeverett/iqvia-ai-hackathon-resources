5dc10917dde1bc12d4632bd2c7525b83cd2cee51
Jason Everett <jev@Jasons-MacBook-Pro.local>
2025-07-10 09:59:58 -0400

    fix

--- activate_tools.py
+++ activate_tools.py
@@ -30,6 +30,19 @@ print(applications)
 
 agent_studio_application_candidates = list(filter(lambda x: x['name'] == "Agent Studio", applications))
 assert len(agent_studio_application_candidates) == 1, "There should be exactly one Agent Studio application in the project"
+agent_studio_endpoint = f"https://{agent_studio_application_candidates[0]['subdomain']}.{os.getenv('CDSW_DOMAIN')}"
+print(f"Agent Studio endpoint: {agent_studio_endpoint}")
 
+# Get current tool templates
+resp = requests.get(
+    f"{agent_studio_endpoint}/api/grpc/listToolTemplates",
+    headers={
+        "Authorization": f"Bearer {os.getenv('CDSW_APIV2_KEY')}"
+    }
+)
+current_tool_templates: list[dict] = resp.json()['tool_templates']
+print(current_tool_templates)
+
+# Activate tool templates
 for tool_template in manifest['tool_templates']:
     print(f"Activating tool template: {tool_template['name']}")
